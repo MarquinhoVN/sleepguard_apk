e35b70d07b7408a20ead98845ba67c4ee37e45c5
Dionatan <dionatanribeiro.netrr@gmail.com>
2025-02-28 14:55:35 -0400

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sleepguard_app/app/app.dart';
 
-void main() {
-  runApp(const SleepguardApp());
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const SleepguardApp(),);
 }
 
 
